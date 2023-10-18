25583b435319ba15325555de8d9cbfab9f8a6983
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-10-18 09:05:06 -0700

    Fanlarga yangi fan qo'shish

--- malumot/views.py
+++ malumot/views.py
@@ -20,12 +20,19 @@ def yonalish_ochir(request, son):
 
 
 def hamma_fanlar(request):
+    if request.method == 'POST':
+        Fan.objects.create(
+            nom=request.POST.get("nom"),
+            yonalish=Yonalish.objects.get(id=request.POST.get("yonalish")),
+            asosiy=request.POST.get("asosiy") == "on",
+        )
     soz = request.GET.get("qidirish_sozi")
     natija = Fan.objects.all()
     if soz:
         natija = natija.filter(nom__contains=soz)
     content = {
-        "fanlar": natija
+        "fanlar": natija,
+        "yonalishlar": Yonalish.objects.all()
     }
     return render(request, "hamma_fanlar.html", content)
 
