5f4821c5ffc782d1b0cd29e3bc2fb7f8245f43ec
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2023-10-26 12:17:56 -0700

    Admin panelidagi UstozAdmin ni ism bo'yicha qidirish imkoniyatini o'zgartirishlar

--- malumot/admin.py
+++ malumot/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import *
 
+
+@admin.register(Ustoz)
+class UstozAdmin(admin.ModelAdmin):
+    list_display = ["id", "ism", "jins", "yosh", "daraja", "fan"]
+    list_display_links = ["id", "ism"]
+    search_fields = ["ism"]
+    search_help_text = "Ism ustunlari bo'yicha qidiring"
+
+
 admin.site.register(Yonalish)
 admin.site.register(Fan)
-admin.site.register(Ustoz)
+# admin.site.register(Ustoz)
